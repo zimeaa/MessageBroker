e9f9602c5f18898cdadde2b9b239da36a2b10661
zimeaa <ximena.alcala.ing@gmail.com>
2025-06-15 01:46:24 -0600

    fix: update server port in app.js and compose.yaml

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 
 app.use(cors({
   origin: '*', // OK si no usas cookies ni auth con credenciales
